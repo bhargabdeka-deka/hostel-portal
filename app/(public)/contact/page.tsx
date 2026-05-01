b5a7ccfa4048babc1d3ce42bc40b101967b7b085
bhargabdeka-deka <143352124+bhargabdeka-deka@users.noreply.github.com>
2026-05-02 04:53:31 +0530

    style: implement official branded multi-colored icons for contact page

--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -29,8 +29,44 @@ export default async function ContactPage() {
     .order('role', { ascending: true });
 
   const mainContacts = [
-    { icon: <MapPin className="w-5 h-5 text-[#EA4335]" />, title: "Location", detail: "Hostel No 7, JEC Road, Jorhat, Assam 785007", color: "text-[#EA4335]", bgColor: "bg-red-50/50", borderColor: "border-red-100", link: "https://www.google.com/maps/search/?api=1&query=Hostel+No+7+Jorhat+Engineering+College" },
-    { icon: <Instagram className="w-5 h-5 text-[#E4405F]" />, title: "Instagram", detail: "@orion_jec", color: "text-[#E4405F]", bgColor: "bg-rose-50/50", borderColor: "border-rose-100", link: "https://www.instagram.com/orion_jec?igsh=azA3enkzZmZ3Y3Fu" },
+    { 
+      icon: (
+        <svg viewBox="0 0 24 24" className="w-6 h-6" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12 2C8.13 2 5 5.13 5 9C5 14.25 12 22 12 22C12 22 19 14.25 19 9C19 5.13 15.87 2 12 2Z" fill="#EA4335"/>
+          <path d="M12 11.5C13.3807 11.5 14.5 10.3807 14.5 9C14.5 7.61929 13.3807 6.5 12 6.5C10.6193 6.5 9.5 7.61929 9.5 9C9.5 10.3807 10.6193 11.5 12 11.5Z" fill="#34A853"/>
+          <path d="M12 22C12 22 5 14.25 5 9C5 8.75 5.01 8.5 5.03 8.25L12 16L18.97 8.25C18.99 8.5 19 8.75 19 9C19 14.25 12 22 12 22Z" fill="#FBBC05"/>
+          <path d="M12 2C15.87 2 19 5.13 19 9C19 9.25 18.99 9.5 18.97 9.75L12 2L5.03 9.75C5.01 9.5 5 9.25 5 9C5 5.13 8.13 2 12 2Z" fill="#4285F4"/>
+        </svg>
+      ), 
+      title: "Location", 
+      detail: "Hostel No 7, JEC Road, Jorhat, Assam 785007", 
+      color: "text-[#EA4335]", 
+      bgColor: "bg-red-50/50", 
+      borderColor: "border-red-100", 
+      link: "https://www.google.com/maps/search/?api=1&query=Hostel+No+7+Jorhat+Engineering+College" 
+    },
+    { 
+      icon: (
+        <svg viewBox="0 0 24 24" className="w-6 h-6" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <linearGradient id="instaGradient" x1="0%" y1="100%" x2="100%" y2="0%">
+              <stop offset="0%" stopColor="#f09433" />
+              <stop offset="25%" stopColor="#e6683c" />
+              <stop offset="50%" stopColor="#dc2743" />
+              <stop offset="75%" stopColor="#cc2366" />
+              <stop offset="100%" stopColor="#bc1888" />
+            </linearGradient>
+          </defs>
+          <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" fill="url(#instaGradient)"/>
+        </svg>
+      ), 
+      title: "Instagram", 
+      detail: "@orion_jec", 
+      color: "text-[#E4405F]", 
+      bgColor: "bg-rose-50/50", 
+      borderColor: "border-rose-100", 
+      link: "https://www.instagram.com/orion_jec?igsh=azA3enkzZmZ3Y3Fu" 
+    },
   ];
 
   return (
